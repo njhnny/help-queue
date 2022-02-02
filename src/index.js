c0c7e1d9c9b8e584f95e4bf5d984d6b2ab6db2dc
Nick Hennessy <njhnny@gmail.com>
2022-02-01 16:59:40 -0800

    create redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
